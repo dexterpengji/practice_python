e7a8d91ed8d9f8631a983cdb0e6820c856b92b1e
dexterpengji <dexterpengji@hotmail.com>
2019-08-26 20:48:28 -0500

    add showListNode function add showListNodeBackward function

--- basics/010_singleLinkedList.py
+++ basics/010_singleLinkedList.py
@@ -12,3 +12,28 @@ l_3 = l_2.next = ListNode(3)
 print(l_1.val)
 print(l_1.next.val)
 print(l_1.next.next.val)
+
+
+def showListNode(LN):
+    while LN:
+        print(LN.val)
+        LN = LN.next
+
+
+def showListNodeBackward(LN):
+    if LN is None:
+        return
+    showListNodeBackward(LN.next)
+    print(LN.val)
+
+
+list1 = ListNode(1)
+list1.next = ListNode(2)
+list1.next.next = ListNode(4)
+
+list2 = ListNode(1)
+list2.next = ListNode(3)
+list2.next.next = ListNode(4)
+
+showListNode(list1)
+showListNodeBackward(list2)
