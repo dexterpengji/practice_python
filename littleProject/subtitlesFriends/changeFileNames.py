cb8c1a89a7c0a16bc64722d4a03f11675a229711
dexterpengji <dexterpengji@hotmail.com>
2019-09-01 01:05:19 -0500

    use this python script to changes subtitles files name

--- littleProject/subtitlesFriends/changeFileNames.py
+++ littleProject/subtitlesFriends/changeFileNames.py
@@ -1,1 +1,18 @@
+import os
+path = 'source/'
 
+# get the list of file names
+fileList = os.listdir(path)
+
+# change names
+for i, x in enumerate(fileList):
+    # old file name with path
+    name_old = path + os.sep + x
+
+    # new file name, and add path
+    filename_new = x.split(".")[1] + ".ass"
+    name_new = path + os.sep + filename_new
+
+    # change names
+    os.rename(name_old, name_new)
+    print(name_old, ' ===> ', name_new)
