b2519af9a6c49b5183de259812e86668921e845b
dexterpengji <dexterpengji@hotmail.com>
2020-01-14 08:55:58 -0600

    fix issue

--- sensorRead/GPS.py
+++ sensorRead/GPS.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import serial
 import pynmea2
 import time
@@ -7,12 +8,17 @@ port = "/dev/ttyACM0"
 def parseGPS(input_data):
 	flag_parse = "GGA"
 	if input_data.find(flag_parse) > 0:
-		msg = pynmea2.parse(input_data)
-		time_computer = "%s %s %s" % (time.gmtime().tm_hour, time.gmtime().tm_min, time.gmtime().tm_sec)
-		data_gps = (time_computer, msg.timestamp, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units, msg.num_sats)
-		print("Computer time: %s | GPS time: %s | Lat: %s %s | Lon: %s %s | Alt: %s %s | Satellites: %s" % data_gps, end="\r")
-		#print(input_data)
-		#print(msg)
+		try:
+			msg = pynmea2.parse(input_data)
+			time_SYS = "%s:%s:%s" % (time.gmtime().tm_hour, time.gmtime().tm_min, time.gmtime().tm_sec)
+			time_GPS = msg.timestamp
+			posi_LAT = "%s%s" % ('-' if msg.lat_dir == "S" else '', msg.lat)
+			posi_LON = "%s%s" % ('-' if msg.lon_dir == "W" else '', msg.lon)
+			data_gps = (time_SYS, time_GPS, posi_LAT, posi_LON, msg.altitude, msg.altitude_units, msg.num_sats)
+			print("SYS time: %s | GPS time: %s | Lat: %s | Lon: %s | Alt: %s %s | Sat: %s" % data_gps)
+		except:
+			print(input_data)
+
 
 serialPort = serial.Serial(port, baudrate = 9600, timeout = 0.5)
 while True:
